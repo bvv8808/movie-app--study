8c8c13f2a00c47d267036e7c5b83f61032347967
Hyeonwoo <hyeonwoo.v24@gmail.com>
2021-02-17 01:12:33 +0900

    Add Router
    date: 2021-0217

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./Screens/Home";
+import About from "./Screens/About";
+import Detail from "./Screens/Detail";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    console.log(movies);
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                year={movie.year}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" exact={true} component={About} />
+      <Route path="/movie/:id" exact={true} component={Detail} />
+    </HashRouter>
+  );
+};
---
+++ src/Screens/About.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default () => {
+  return <span>This is About page.</span>;
+};
